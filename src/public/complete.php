23037711d1bb7ba48e393cf06e8f53423bf87450
w-shaila <w-shaulabg.2952r-3@outlook.jp>
2022-09-16 20:26:40 +0900

    不要コメントアウト削除

--- src/public/complete.php
+++ src/public/complete.php
@@ -14,7 +14,7 @@ $dbPassword = 'password';
 $pdo = new PDO(
   'mysql:host=mysql; dbname=contactform; charset=utf8',$dbUserName, $dbPassword);
   $stmt = $pdo->prepare("INSERT INTO contacts (title, email, content)
-   VALUES (:title, :email, :content)");//並び順にカラムに追加
+   VALUES (:title, :email, :content)");
    
   $stmt->bindParam( ':title', $title, PDO::PARAM_STR);
   $stmt->bindParam( ':email', $email, PDO::PARAM_STR);
